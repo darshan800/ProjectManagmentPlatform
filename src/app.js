143421ffa2c6545c49ae35dd809b439d1187d565
Darshan K <darshankulkarni34@gmail.com>
2026-02-26 02:24:11 +0530

    feat: implement healthcheck route and controller with v1 versioning

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 
@@ -9,12 +9,19 @@ app.use(express.urlencoded({ extended: "true", limit: "16kb" }));
 app.use(express.static("public"));
 
 // cors configuration
-app.use(cors({
-  origin:process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
-  credentials:true,
-  methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
-  allowedHeaders:["Content-Type","Authorization"]
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+// import the routes : this things are done after cors and middleware configuration
+//since the export type is default from heathche.router.js file, so we can name anything while importing
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 app.get("/", (req, res) => {
   res.send("welcome to basecampy!!");
