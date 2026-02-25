1dd6e7a9f3283d935bdf23d4fd0367131e44c77f
Darshan K <darshankulkarni34@gmail.com>
2026-02-26 03:04:28 +0530

    feat: implement async-handler utility for centralized error handling

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,11 +1,21 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthcheck = (req, res) => {
-  try {
-    res
-      .status(200)
-      .json(new ApiResponse(200, { message: "Server is running" }));
-  } catch (error) {}
-};
+// to avoid too many try catch block we use async await
+// const healthcheck = async (req, res, next) => {
+//   try {
+//     const user = await getuserfromDB();
+//     res
+//       .status(200)
+//       .json(new ApiResponse(200, { message: "Server is running" }));
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+//second method
+const healthcheck = asyncHandler(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "server is still  running" }));
+});
 
 export { healthcheck };
