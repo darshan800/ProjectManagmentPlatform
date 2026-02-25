90804098bf6eb19bbe1ad31ceec805aa189acf29
Darshan K <darshankulkarni34@gmail.com>
2026-02-26 01:20:34 +0530

    feat: verify mongodb atlas connection and setup environment variables

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-  console.log(`Server is listening at port: http://localhost:${port}`),
-);
+connectDB()
+  .then(() => {
+    app.listen(port, () =>
+      console.log(`Server is listening at port: http://localhost:${port}`),
+    );
+  })
+  .catch((err) => {
+    console.err("Mongod connection error", err);
+    process.exit(1);
+  });
