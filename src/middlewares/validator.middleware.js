6fd9e74553b8da6036346db20ca15b350612d1a7
Darshan K <darshankulkarni34@gmail.com>
2026-03-09 15:41:11 +0530

    feat: add express-validator middleware and user registration rules

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,22 @@
+import { validationResult } from "express-validator";
+import { ApiError } from "../utils/api-error";
+
+
+// so the task is , i'll give you some file , you need to extract error from it and
+// you will just process them,
+export const validate = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const extractedErros = [];
+  errors.array().map((err) =>
+    extractedErros.push({
+      [err.path]: err.msg,
+    }),
+  );
+
+  throw new ApiError(422, "Rececived data is not valid",extractedErros)
+
+};
